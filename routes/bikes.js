f54a86a2eee110743b23eaf19e149d8a5435d61f
w0531640 <w0531640@nscc.ca>
2025-10-10 20:58:18 -0300

    implemented bikes.js routes w/o Joi validation yet

--- routes/bikes.js
+++ routes/bikes.js
@@ -1,24 +1,83 @@
 import express from "express"
+import { v4 as uuidv4 } from "uuid"
+import b from "../artifacts/json-server-test.json" with { type: "json" }
 const router = express.Router()
 
+let bikes = b
+
 router.get("/", (req, res) => {
-  res.send("GET ALL Bikes")
+  if (!bikes || bikes.length === 0) {
+    return res.status(404).json({ message: "No stuff found" });
+  }
+  res.status(200).json(bikes);
 })
 
 router.get("/:id", (req, res) => {
-  res.send(`ID: ${req.params.id}`)
+  const id = bikes.find((item) => {
+    return item.id === req.params.id
+  })
+
+  if (!id) {
+    return res.status(404).send({ message: "ID not found" })
+  }
+
+  res.status(200).json(id)
 })
 
 router.post("/", (req, res) => {
-  res.json(req.body);
+  // Error handling
+  // const { error } = validatePerson(req.body);
+  // if (error) {
+  //   const errors = error.details.map(item => item.message)
+  //   return res.status(400).json(errors)
+  // }
+
+  const uuid = uuidv4()
+  req.body.id = uuid;
+  bikes.push(req.body)
+  res.status(201).json(req.body);
 })
 
 router.put("/:id", (req, res) => {
-  res.json(req.body);
+  const id = req.params.id
+
+  const updatedBody = req.body
+
+  // Error handling
+  // const { error } = validatePerson(updatedBody);
+  // if (error) {
+  //   const errors = error.details.map(item => item.message)
+  //   return res.status(400).json(errors)
+  // }
+
+  const bodyIndex = bikes.find((item) => {
+    return item.id === id;
+  })
+
+  if (!id || !bodyIndex) {
+    return res.status(404).send({ message: "ID not found" })
+  }
+
+  req.body.id = id;
+
+  const bodyToUpdateIndex = bikes.indexOf(bodyIndex)
+
+  bikes[bodyToUpdateIndex] = { id, ...updatedBody }
+
+  res.status(200).json(bikes[bodyToUpdateIndex])
 })
 
 router.delete("/:id", (req, res) => {
-  res.json(req.body);
+  const filteredBody = bikes.filter((item) => {
+    return item.id !== req.params.id
+  })
+
+  if (filteredBody.length === bikes.length) {
+    res.status(404).send({ error: "Deletion unsuccessful" })
+  } else {
+    bikes = filteredBody
+    res.status(200).json({ message: "Body deleted successfully" })
+  }
 })
 
 export default router;
