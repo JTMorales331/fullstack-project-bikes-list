c1ad3cf0561bdcd1cfc6c3b641698155036a249a
w0531640 <w0531640@nscc.ca>
2025-10-10 10:22:36 -0300

    did stuffz

---
+++ routes/bikes.js
@@ -0,0 +1,24 @@
+import express from "express"
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  res.send("GET ALL Bikes")
+})
+
+router.get("/:id", (req, res) => {
+  res.send(`ID: ${req.params.id}`)
+})
+
+router.post("/", (req, res) => {
+  res.json(req.body);
+})
+
+router.put("/:id", (req, res) => {
+  res.json(req.body);
+})
+
+router.delete("/:id", (req, res) => {
+  res.json(req.body);
+})
+
+export default router;
