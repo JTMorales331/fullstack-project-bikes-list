d8723f5ff461c94d316a20194518c77b4e067cd9
w0531640 <w0531640@nscc.ca>
2025-12-09 21:51:15 -0400

    added sendFile

--- app.js
+++ app.js
@@ -49,6 +49,10 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'))
+})
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
